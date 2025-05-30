aa6331c60b9128a0c300ddb36abd9ec371775641
astroC86 <66444189+astroC86@users.noreply.github.com>
2025-05-31 01:43:49 +0200

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // lazy_abs
 SEXP lazy_abs(SEXP x);
 RcppExport SEXP _altrepisode_lazy_abs(SEXP xSEXP) {
